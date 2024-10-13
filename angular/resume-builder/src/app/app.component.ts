8e5bca54decade00b528abe26520dfe45e276a81
Avinash Ranjalkar <avinash.ranjalkar@gmail.com>
2024-10-12 23:56:24 -0400

    added some transitions and new route for contact

--- angular/resume-builder/src/app/app.component.ts
+++ angular/resume-builder/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { ResumeComponent } from "./resume/resume.component";
+import { ContactComponent } from './contact/contact.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ResumeComponent],
+  imports: [RouterOutlet, NavbarComponent, ResumeComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular/resume-builder/src/app/app.routes.ts
+++ angular/resume-builder/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
-import {ResumeComponent} from './resume/resume.component'
+import { ResumeComponent } from './resume/resume.component'
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
-    { path: 'resume', component: ResumeComponent }
+    { path: 'resume', component: ResumeComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'about', component: AboutComponent }
 ];
